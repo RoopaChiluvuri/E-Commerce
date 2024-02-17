d1156d70e507d453031b2c4f965a9521d7f304ca
RoopaChiluvuri <roopasree2509@gmail.com>
2024-02-16 17:56:56 -0800

    Added ProductDisplay

---
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './ProductDisplay.css'
+
+export const ProductDisplay = (props) => {
+  return (
+    <div className='productdisplay'>
+        <div className='productdisplay-left'>
+            <div className='productdisplay-img-list'>
+
+            </div>
+        </div>
+        <div className='productdisplay-right'>
+
+        </div>
+    </div>
+  )
+}
