de2e0867d7ac02fd226c46d7d5425b7cbbc0d446
RoopaChiluvuri <roopasree2509@gmail.com>
2024-02-21 18:07:03 -0800

    Added Cart Functionality

--- src/Components/CartItems/CartItems.jsx
+++ src/Components/CartItems/CartItems.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from 'react'
 import { ShopContext } from '../../Context/ShopContext'
 import './CartItems.css'
+import remove_icon from '../Assets/cart_cross_icon.png';
 
 export const CartItems= () => {
   const {all_product, cartItems, removeFromCart} = useContext(ShopContext)
@@ -15,11 +16,22 @@ export const CartItems= () => {
         <p>Remove</p>
       </div>
       <hr />
-      <div>
-        <div className='cartitems-format'>
-          <img src="" alt="" className='carticon-product-icon' />
-        </div>
-      </div>
+      {all_product.map((e)=>{
+        if(cartItems[e.id]>0)
+        {
+          return <div>
+                   <div className='cartitems-format cartitems-format-main'>
+                      <img src={e.image} alt="" className='carticon-product-icon' />
+                      <p>{e.name}</p>
+                      <p>${e.new_price}</p>
+                      <button className='cartitems-quantity'>{cartItems[e.id]}</button>
+                      <p>{e.new_price*cartItems[e.id] }</p>
+                      <img className='cartitems-remove-icon' src={remove_icon} onClick={()=>{removeFromCart(e.id)}} alt="" />
+                    </div>
+                     <hr />
+                   </div>
+        }
+      })}
     </div>
   )
 }
